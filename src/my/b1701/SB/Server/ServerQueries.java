b7fd86e78a8dc0dbe9a2823e1f5a90d65269fb75
arpit87 <arpit87@gmail.com>
2012-09-08 14:34:29 -0700

    redesigned!!!

---
+++ src/my/b1701/SB/Server/ServerQueries.java
@@ -0,0 +1,14 @@
+package my.b1701.SB.Server;
+
+public class ServerQueries {
+	
+
+	public static  String  SERVER_ADDRESS = "http://www.greenyatra.org/sb/";
+	
+	public static String GET_QUERY=SERVER_ADDRESS+"/getNearby";
+	
+	public static final String POST_QUERY = SERVER_ADDRESS;
+	public static final String DELETE_USER_QUERY = SERVER_ADDRESS;
+	public static final String GET_USERS_QUERY = SERVER_ADDRESS;
+
+}
